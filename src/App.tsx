f5446f1247cf060d17f82fa7400b1a2a0f1ecb97
keyurparalkar <keur.plkar@gmail.com>
2021-07-28 07:14:49 +0530

    styles: added basic styles to the dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import {
   Grid,
   Input,
   Spinner,
-  theme
+  Stat,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  theme,
 } from "@chakra-ui/react";
 import _ from "lodash";
 import * as React from "react";
@@ -20,18 +25,20 @@ export const App = () => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
 
-    if(event.target.value!== ""){
-      const debounceTempCall =_.debounce(() => {
+    if (event.target.value !== "") {
+      const debounceTempCall = _.debounce(() => {
         refetch();
       }, 1000);
 
       debounceTempCall();
     } else {
-      queryClient.removeQueries("getRepos", {exact: true});
+      queryClient.removeQueries("getRepos", { exact: true });
     }
   };
 
-  const { isLoading, isError, data, refetch, isSuccess} = useGetRepos(() => getReposAxios(value));
+  const { isLoading, isError, data, refetch, isSuccess } = useGetRepos(() =>
+    getReposAxios(value)
+  );
 
   return (
     <ChakraProvider theme={theme}>
@@ -45,48 +52,59 @@ export const App = () => {
             size="sm"
           />
           <ul>
-            {isLoading  && <Spinner />}
-            {isSuccess && data ? (data as any).items.map((item: any, index: number) => (
+            {isLoading && <Spinner />}
+            {isSuccess && data
+              ? (data as any).items.map((item: any, index: number) => (
                   <li key={`v--${index}`}>
                     <span>{item?.full_name}</span>
                     &nbsp;
-                    <span>{item?.forks_count}</span>
-                    &nbsp;
-                    <span>{item?.stargazers_count}</span>
-                    &nbsp;
-                    <span>{item?.watchers_count}</span>
-                    &nbsp;
-                    <span>{item?.watchers_count}</span>
-                    &nbsp;
-                    <span>{item?.open_issues_count}</span>
-                  </li>
-                )) : "No data found. Please search for repos."
-              }
-
-            {isError && "Error is displaying data."}
-            {/* {!isLoading ? (
-              data ? (
-                (data as any).items.map((item: any, index: number) => (
-                  <li key={`v--${index}`}>
-                    <span>{item?.full_name}</span>
-                    &nbsp;
-                    <span>{item?.forks_count}</span>
-                    &nbsp;
-                    <span>{item?.stargazers_count}</span>
-                    &nbsp;
-                    <span>{item?.watchers_count}</span>
-                    &nbsp;
-                    <span>{item?.watchers_count}</span>
-                    &nbsp;
-                    <span>{item?.open_issues_count}</span>
+                    <StatGroup>
+                      <Stat>
+                        <Box
+                          borderRadius="5px"
+                          borderWidth="1px"
+                          boxShadow="xs"
+                        >
+                          <StatLabel>Forks</StatLabel>
+                          <StatNumber>{item?.forks_count}</StatNumber>
+                        </Box>
+                      </Stat>
+                      <Stat>
+                        <Box
+                          borderRadius="5px"
+                          borderWidth="1px"
+                          boxShadow="xs"
+                        >
+                          <StatLabel>Stars</StatLabel>
+                          <StatNumber>{item?.stargazers_count}</StatNumber>
+                        </Box>
+                      </Stat>
+                      <Stat>
+                        <Box
+                          borderRadius="5px"
+                          borderWidth="1px"
+                          boxShadow="xs"
+                        >
+                          <StatLabel>Watchers</StatLabel>
+                          <StatNumber>{item?.watchers_count}</StatNumber>
+                        </Box>
+                      </Stat>
+                      <Stat>
+                        <Box
+                          borderRadius="5px"
+                          borderWidth="1px"
+                          boxShadow="xs"
+                        >
+                          <StatLabel>Open Issue Count</StatLabel>
+                          <StatNumber>{item?.open_issues_count}</StatNumber>
+                        </Box>
+                      </Stat>
+                    </StatGroup>
                   </li>
                 ))
-              ) : (
-                "No data found. Please search for repos."
-              )
-            ) : (
-              <Spinner />
-            )} */}
+              : "No data found. Please search for repos."}
+
+            {isError && "Error is displaying data."}
           </ul>
         </Grid>
       </Box>
